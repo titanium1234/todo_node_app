258392906aa071ffbe5d15eed10d897cc4d51534
titanium <titaniumdev123@gmail.com>
2021-12-21 15:18:28 +0000

    done with todo node api

--- express_app.js
+++ express_app.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const todoController = require('./controllers/todoController');
+const PORT = process.env.PORT || 4000;
 const server = express();
 // const mongo_db_url = '';
 // const mongo_db_url = 'mongodb://localhost/todos_db';
 
 server.use(express.json());
 
-server.listen(4000, function(){
+server.listen(PORT, function(){
     console.log('Server has started to run in express');
     mongoose.connect(process.env.ATLAS_URL)
     .then(function(){
         console.log('DB is connected');
+        server.get('/', function(req, res){
+            res.status(200).json({success: true, message: 'WELCOME, this is Isaac todo node API'});
+        });
         server.get('/todos', todoController.getAllTodos);
         server.get('/todo/:id', todoController.getTodoById);
         server.post('/todo', todoController.insertTodo);
