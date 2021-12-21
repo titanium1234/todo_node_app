0e53f9276e0d6adc5a9844b3cb9debcedfe59234
titanium <titaniumdev123@gmail.com>
2021-12-21 16:28:58 +0000

    final commit v2

--- express_app.js
+++ express_app.js
@@ -6,20 +6,20 @@ const todoController = require('./controllers/todoController');
 const PORT = process.env.PORT || 4000;
 
 server.use(express.json());
+server.get('/', function(req, res){
+    res.status(200).json({success: true, message: 'WELCOME, this is Isaac todo node API'});
+});
+server.get('/todos', todoController.getAllTodos);
+server.get('/todo/:id', todoController.getTodoById);
+server.post('/todo', todoController.insertTodo);
+server.put('/todo/:id', todoController.updateTodoById);
+server.delete('/todo/:id', todoController.deleteTodoById);
 
 server.listen(PORT, function(){
     console.log('Server has started to run...');
     mongoose.connect(process.env.DB_URL)
     .then(function(){
         console.log('DB is connected');
-        server.get('/', function(req, res){
-            res.status(200).json({success: true, message: 'WELCOME, this is Isaac todo node API'});
-        });
-        server.get('/todos', todoController.getAllTodos);
-        server.get('/todo/:id', todoController.getTodoById);
-        server.post('/todo', todoController.insertTodo);
-        server.put('/todo/:id', todoController.updateTodoById);
-        server.delete('/todo/:id', todoController.deleteTodoById);
     })
     .catch(function(error){
         console.log('DB is not connected: ', error.message);
